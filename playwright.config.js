3b59b068f7850f622a8b917c420ec5d556cc35dd
Luis <Luistejeda0011@gmail.com>
2022-10-16 20:33:38 -0300

    Fixing e2e error in pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -17,13 +17,13 @@ const config = {
     url: 'http://localhost:5000/',
     timeout: 10000
   },
-  timeout: 30 * 1000,
+  timeout: 100 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 100000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
